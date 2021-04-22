996183cbff4828bfbeedb9bca27f54a622b066c6
史 翔新 <x_shi@yumemi.co.jp>
2021-04-22 20:55:18 +0900

    init project

---
+++ Shared/ScrollViewReaderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReaderDemoApp.swift
+//  Shared
+//
+//  Created by 史 翔新 on 2021/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReaderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
